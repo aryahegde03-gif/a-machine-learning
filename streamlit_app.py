7cf107a7d3552744ed8687c128a686d2870fa147
Arya <aryahegde03@gmail.com>
2026-03-22 19:14:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Machine learning app')
 
 st.info('this is a machine learning app')
+
+df=pd.read_csv('https://raw.githubusercontent.com/aryahegde03-gif/a-machine-learning/refs/heads/master/customers-100.csv')
+df
